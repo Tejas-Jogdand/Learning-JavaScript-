fd31d6febaa9c13597a6f4acc5b669f0ae5db144
Tejas Jogdand <tejasjogdand37@gmail.com>
2025-01-15 23:13:38 +0530

    Revised 50%

--- 08 Chapter2 PS/tempCodeRunnerFile.js
+++ 08 Chapter2 PS/tempCodeRunnerFile.js
@@ -1,8 +1,10 @@
-let age= parseInt(prompt("What is your age ? "));
+const prompt = require("prompt-sync")();
 
-// if(age>=10 && age <=20){
-//     console.log("Your age lies in between 10 to 20");
-// }
-// else{
-//     console.log("Your age doesnt lie in between 10 to 20");
-// }
+let age= Number.parseInt(prompt("What is your age ? "));
+
+if(age>=10 && age <=20){
+    console.log("Your age lies in between 10 to 20");
+}
+else{
+    console.log("Your age doesnt lie in between 10 to 20");
+}
